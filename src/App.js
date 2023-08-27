645d0dbc6db15310c1030561ac70d6e162cf734f
Anand Prakash Gupta <127001977+Anand1233@users.noreply.github.com>
2023-08-27 19:44:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -7,17 +7,17 @@ function App() {
     <h2>LOGIN PAGE</h2>
     <div className='container'>
       <form className='box'>   
-     <input className='username' type="text" placeholder='Username'   >
+     <input  className='username' type="text" placeholder='Username'   >
       </input>
       <br/>
-      <input className='password' type="password" placeholder='Password' >
+      <input  className='password' type="password" placeholder='Password' >
 
       </input>
       <br/>
-      <button className='button' type='submit'>Login
+      <button className='button' type='submit'>Sign In
       </button>
       <br/>
-      <a href="http://127.0.0.1:5500/src/verify.html" target="_blank" > <button className='forgetbtn'>Forgot Password</button> </a>
+      <a href='http://127.0.0.1:5500/src/verify.html' className='forgetbtn' target='_blank' >Forgot Password?</a>
       </form>
       </div>
      </>
